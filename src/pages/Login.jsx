0c3a2bc6e1598d53b7390d74e81c46c4567978ca
Nam Tran Trong <nam.tran-trong@ubisoft.com>
2023-11-13 22:30:11 +0700

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,40 @@
 import { BoxInput } from "../components/Input/BoxInput"
 import HeaderLogin from "../components/header/HeaderLogin"
-
+import '../styles/Pages/Login.css';
 export const Login = () => {
     return(
         <div>
             <HeaderLogin/>
-            <BoxInput/>
+            <div className="content">
+                <div className="content_left">
+                    <img className="background" src="./images/background/background_login.png" alt="background"/>
+                </div>
+                <div className="content_right">
+                    <form className="form-login">
+                        <div>Welcome back, Yash</div>
+                        <div>Welcome back!Please enter your details</div>
+                        <BoxInput/>
+                        <BoxInput/>
+                        <div>error</div>
+                        <div>
+                            <label>
+                                <input
+                                type="checkbox"
+                                />
+                                Checkbox Label
+                            </label>
+                            <a>Forgot Password</a>
+                        </div>
+                        <button>Login</button>
+                        <div>
+                            You don't have an account? 
+                            <a>
+                                Sign up for free    
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
 
     )
