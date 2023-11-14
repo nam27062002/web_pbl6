f9efa8243cc52d7222d3c40f07b9741f7918b071
nam tran trong <nam.tran-trong@ubisoft.com>
2023-11-14 14:33:33 +0700

    update

--- src/components/Input/BoxInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../../styles/Input/BoxInput.css';
-export const BoxInput = () => {
-    return (
-        <div className="box_input">
-            <input className="input" placeholder="Email"/>
-        </div>
-    );
-}
