5b5d78641a6e5fa689b3fd7af06930a76973f27b
nam tran trong <nam.tran-trong@ubisoft.com>
2023-11-14 14:33:05 +0700

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,41 @@
-import { BoxInput } from "../components/Input/BoxInput"
 import HeaderLogin from "../components/header/HeaderLogin"
 import '../styles/Pages/Login.css';
+import React, { useState } from 'react';
 export const Login = () => {
+    
+    const [isEmailFocused, setIsEmailFocused] = useState(false);
+    const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleEmailFocus = () => {
+        setIsEmailFocused(true);
+        setIsPasswordFocused(false);
+    };
+
+    const handlePasswordFocus = () => {
+        setIsEmailFocused(false);
+        setIsPasswordFocused(true);
+    };
+
+    const handleInputBlur = () => {
+        setIsEmailFocused(false);
+        setIsPasswordFocused(false);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
     return(
         <div>
             <HeaderLogin/>
@@ -10,11 +44,28 @@ export const Login = () => {
                     <img className="background" src="./images/background/background_login.png" alt="background"/>
                 </div>
                 <div className="content_right">
-                    <form className="form-login">
-                        <div>Welcome back, Yash</div>
-                        <div>Welcome back!Please enter your details</div>
-                        <BoxInput/>
-                        <BoxInput/>
+                    <form className="form-login" onSubmit={handleSubmit}>
+                        <div className="form_title_1">Welcome back, Yash</div>
+                        <div className="form_title_2">Welcome back!Please enter your details</div>
+                        <div className="box_input" style={{ borderBottom: `1px solid ${isEmailFocused ? 'green' : 'rgb(169, 156, 156)'}` }}>
+                            <input
+                                className="input"
+                                placeholder="Email"
+                                onFocus={handleEmailFocus}
+                                onBlur={handleInputBlur}
+                                onChange={handleEmailChange}
+                                value={email}/>
+                        </div>
+                        <div className="box_input" style={{ borderBottom: `1px solid ${isPasswordFocused ? 'green' : 'rgb(169, 156, 156)'}` }}>
+                                <input
+                                    className="input"
+                                    placeholder="Password"
+                                    type="password"
+                                    onFocus={handlePasswordFocus}
+                                    onBlur={handleInputBlur}
+                                    onChange={handlePasswordChange}
+                                    value={password}/>
+                        </div>
                         <div>error</div>
                         <div>
                             <label>
@@ -37,6 +88,4 @@ export const Login = () => {
             </div>
         </div>
 
-    )
-}
-  
+    )}
