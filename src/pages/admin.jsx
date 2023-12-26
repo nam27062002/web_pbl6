6343decc38da9579c7e257cb7c8c620fc7753d31
nam tran trong <nam.tran-trong@ubisoft.com>
2023-12-26 10:02:52 +0700

    update

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -175,7 +175,7 @@ export const Admin = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const handleApprove = async () => {
         try {
-            const response = await fetch(`http://ridewizard.pro:9000/api/v1/drivers/driver/:type?user_id=149&type=5`, {
+            const response = await fetch(`http://ridewizard.pro:9000/api/v1/drivers/driver/approve?user_id=${selectedItemId}&type=${currentImageIndex + 1}`, {
                 method: 'PUT',
                 headers: {
                     Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQ3LCJlbWFpbCI6ImFkbWluIiwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoicGFzc2VuZ2VyIn0seyJpZCI6Mywicm9sZSI6ImFkbWluIn1dLCJpYXQiOjE3MDI1NDczMDMsImV4cCI6MTcwNTEzOTMwM30.d8eYVYBYE71TAb7OmZ_aPci4YNbBw3-G1lOu7g-l0Ug',
@@ -183,13 +183,14 @@ export const Admin = () => {
             });
 
             if (!response.ok) {
-                throw new Error('Failed to fetch data');
+                throw new Error('Failed to approve image');
             }
 
-            const data = await response.json();
-            console.log(data);
+            console.log('Image approved');
+
+            await getListPhotoVerifyData(selectedItemId);
         } catch (error) {
-            console.error('Error fetching data:', error);
+            console.error('Error denying image:', error);
         }
     };
 
@@ -197,7 +198,7 @@ export const Admin = () => {
 
     const handleDeny = async () => {
         try {
-            const response = await fetch(`http://ridewizard.pro:9000/api/v1/drivers/driver/:type?user_id=${selectedItemId}&type=${currentImageIndex + 1}`, {
+            const response = await fetch(`http://ridewizard.pro:9000/api/v1/drivers/driver/deny?user_id=${selectedItemId}&type=${currentImageIndex + 1}`, {
                 method: 'PUT',
                 headers: {
                     Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQ3LCJlbWFpbCI6ImFkbWluIiwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoicGFzc2VuZ2VyIn0seyJpZCI6Mywicm9sZSI6ImFkbWluIn1dLCJpYXQiOjE3MDI1NDczMDMsImV4cCI6MTcwNTEzOTMwM30.d8eYVYBYE71TAb7OmZ_aPci4YNbBw3-G1lOu7g-l0Ug',
