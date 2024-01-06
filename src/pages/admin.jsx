9d0bddf0c60d47db2192aa76ac82dd14f5ca59de
Duong Bao <duongbao.08012001@gmail.com>
2024-01-06 07:33:08 +0700

    last commit v3

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -35,7 +35,7 @@ export const Admin = () => {
             const response = await fetch(`http://ridewizard.pro:9000/api/v1/drivers/identification/${id}`, {
                 method: 'GET',
                 headers: {
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQ3LCJlbWFpbCI6ImFkbWluIiwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoicGFzc2VuZ2VyIn0seyJpZCI6Mywicm9sZSI6ImFkbWluIn1dLCJpYXQiOjE3MDI1NDczMDMsImV4cCI6MTcwNTEzOTMwM30.d8eYVYBYE71TAb7OmZ_aPci4YNbBw3-G1lOu7g-l0Ug',
+                    Authorization: `Bearer ${user.accessToken}`,
                 },
             });
             console.log("admin " + response);
