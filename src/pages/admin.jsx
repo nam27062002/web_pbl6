12cbb59bb984e52b4c2b84b9d29700cb5902fce6
Nam Tran Trong <nam.tran-trong@ubisoft.com>
2024-01-06 07:37:39 +0700

    update

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -257,7 +257,7 @@ export const Admin = () => {
             const response = await fetch(`http://ridewizard.pro:9000/api/v1/drivers/driver/deny?user_id=${selectedItemId}&type=${currentImageIndex + 1}`, {
                 method: 'PUT',
                 headers: {
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQ3LCJlbWFpbCI6ImFkbWluIiwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoicGFzc2VuZ2VyIn0seyJpZCI6Mywicm9sZSI6ImFkbWluIn1dLCJpYXQiOjE3MDI1NDczMDMsImV4cCI6MTcwNTEzOTMwM30.d8eYVYBYE71TAb7OmZ_aPci4YNbBw3-G1lOu7g-l0Ug',
+                    Authorization: `Bearer ${user.accessToken}`,
                 },
             });
 
